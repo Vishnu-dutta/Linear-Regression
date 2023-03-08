de4df488e4ff70cfd090500457079445c12c3830
Vishnu-dutta <111376662+Vishnu-dutta@users.noreply.github.com>
2023-03-09 02:24:07 +0530

    added train and test sets

--- OneHotEncoding/exericse.py
+++ OneHotEncoding/exericse.py
@@ -3,6 +3,7 @@ from sklearn import linear_model
 from sklearn import preprocessing
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
 import numpy as np
 
 
@@ -19,6 +20,26 @@ y = final["Sell Price($)"]
 reg.fit(X,y)
 
 print(final)
-print(reg.predict([[45000,4,0,1]]))     # mercedez benz that is 4 yr old with mileage 45000
-print(reg.predict([[86000,7,0,0]]))     # BMW X5 that is 7 yr old with mileage 86000
-print(reg.score(X,y))                   # score(accuracy of the model)
+print(reg.predict([[45000,4,0,1]]))                                 # mercedez benz that is 4 yr old with mileage 45000
+print(reg.predict([[86000,7,0,0]]))                                 # BMW X5 that is 7 yr old with mileage 86000
+print("complete dataset score: {}".format(reg.score(X,y)))          # score(accuracy of the model)
+
+# training and testing dataset
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=25)
+print(y_test)
+print(reg.predict(X_test))
+print("trained dataset score: {}".format(reg.score(X_test, y_test)))
+
+'''
+def best_score(x,y):
+    for i in range(30):
+        X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=i)
+        reg = linear_model.LinearRegression()
+        reg.fit(X_train, y_train)
+        a = (reg.score(X_test, y_test))
+        print("score: {}, iteration: {}".format(a,i))
+
+best_score(X,y)
+
+'''
