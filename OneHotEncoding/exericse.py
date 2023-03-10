417d7ef1b7aa24c67e9083c628a5a5ad40d971bf
Vishnu-dutta <111376662+Vishnu-dutta@users.noreply.github.com>
2023-03-11 04:31:08 +0530

    Issues related to dummy variable trap

--- OneHotEncoding/exericse.py
+++ OneHotEncoding/exericse.py
@@ -4,6 +4,7 @@ from sklearn import preprocessing
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
 import numpy as np
 
 
@@ -13,14 +14,26 @@ dummies = pd.get_dummies(df["Car Model"])
 merged = pd.concat([df, dummies], axis="columns")
 final = merged.drop(["Car Model", "BMW X5"], axis="columns")
 
+'''
+Although we talked about dummy variable trap yet we don't see as such in this case. We are getting the same values as
+output
+
+final = merged.drop(["Car Model"], axis="columns")
+X = final.drop(["Sell Price($)"], axis="columns")
+y = final["Sell Price($)"]
+reg.fit(X,y)
+
+print(reg.predict([[45000,4,0,0,1]]))               Mercedes benz that is 4 yr old with mileage 45000
+print(reg.predict([[86000,7,0,1,0]]))               BMW X5 that is 7 yr old with mileage 86000
+'''
+
 reg = linear_model.LinearRegression()
 X = final.drop(["Sell Price($)"], axis="columns")
 y = final["Sell Price($)"]
-
 reg.fit(X,y)
 
 print(final)
-print(reg.predict([[45000,4,0,1]]))                                 # mercedez benz that is 4 yr old with mileage 45000
+print(reg.predict([[45000,4,0,1]]))                                 # Mercedes benz that is 4 yr old with mileage 45000
 print(reg.predict([[86000,7,0,0]]))                                 # BMW X5 that is 7 yr old with mileage 86000
 print("complete dataset score: {}".format(reg.score(X,y)))          # score(accuracy of the model)
 
@@ -30,6 +43,7 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_s
 print(y_test)
 print(reg.predict(X_test))
 print("trained dataset score: {}".format(reg.score(X_test, y_test)))
+# print(df.shape)
 
 '''
 def best_score(x,y):
